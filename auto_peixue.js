c30bda9cfe43f32e93532a0533553ea04c75108d
franklinwa@163.com <franklinwa@163.com>
2016-07-07 00:37:51 +0800

    20160707

--- auto_peixue.js
+++ auto_peixue.js
@@ -52,6 +52,71 @@ Peixue.prototype.ready_data = function(){
 };
 
 
+
+//生成需要的html代码。友情赞助
+Peixue.prototype.auto_html_bussiness = function( arg ){
+    var html;
+
+    if( arg.match( /^友情赞助/ )!= null ){
+
+        html = this.auto_html_icon(7);
+        html += '<p style=" padding: 10px; text-align: center; font-size: 16px; color: #ffffff; font-family: 微软雅黑; background-color: #00B050;">友情赞助</p>';
+
+        html += this.auto_html_icon(0);
+        html += '<p style="white-space: pre-wrap; font-family: &#39;Helvetica Neue&#39;, Helvetica, &#39;Hiragino Sans GB&#39;, &#39;Microsoft YaHei&#39;, Arial, sans-serif; text-indent: 2em; line-height: 2em;">    <span style="color: #7F7F7F; font-size: 14px;">加实战技能<span style="color: #FF0000;">QQ群：470965059</span>，注明来自<span style="color: #FF0000;">微文速递公众号</span>就免费赠送《房建施工实战系列课程》系列课程中<span style="color: #FF0000;">价值348元</span>的课程一套。</span></p><br/><p style="white-space: pre-wrap; font-family: &#39;Helvetica Neue&#39;, Helvetica, &#39;Hiragino Sans GB&#39;, &#39;Microsoft YaHei&#39;, Arial, sans-serif; line-height: 2em; text-align: center;">    <a href="http://mp.weixin.qq.com/s?__biz=MzIyMzAxMDQ3OQ==&mid=2651648469&idx=1&sn=90f3124b6e701839f685e07dce551612&scene=21#wechat_redirect" target="_blank" data_ue_src="http://mp.weixin.qq.com/s?__biz=MzIyMzAxMDQ3OQ==&amp;mid=2651648469&amp;idx=1&amp;sn=90f3124b6e701839f685e07dce551612&amp;scene=21#wechat_redirect" style="color: rgb(96, 127, 166); text-decoration: none;">亲，马上提高房建施工实战技能！</a></p> <br/>';
+
+    }else{
+
+        html = '<p style="margin-top: 10px; margin-bottom: 10px;  text-indent: 2em; line-height: 2em;"><span style="color: #7F7F7F; font-family: 微软雅黑;font-size: 16px;">'+ arg +'</span></p>';
+
+    }
+
+
+    console.log(html);
+
+    return html;
+};
+
+
+
+
+//生成需要的html代码。优质公众号
+Peixue.prototype.auto_html_gonghao = function( arg ){
+
+    var html;
+
+    html = '<p style=" text-align: center; line-height: 3em;"> <span style="font-size: 16px; color: #BFBFBF; font-family: 微软雅黑;">♕<br/></span></p> <section style=" padding: 10px; text-align: center; font-size: 16px; color: #ffffff; font-family: 微软雅黑; background-color: #00B050;">优质公众号</section>';
+
+    html += this.auto_html_icon(0);
+    //    公众号：1
+    html += '<p style="line-height: 2em; text-align: center;"><img data-s="300,640" data-type="jpeg" src="https://mmbiz.qlogo.cn/mmbiz/RHNE9AcLP6Jz4kXcLibtEpUI0cibVibRECSzjyxPId4cysqibD4yMVibicTibvzmThqtduSCnpCBYvwiawrVRdxbLa9frQ/0?wx_fmt=jpeg" data-ratio="1" data-w="344"/><span style="font-size: 14px; color: #888888;"><br/>关注赞助商，关注坚持读。</span></p>';
+    //    公众号：2
+    html += '<p style="line-height: 2em; text-align: center;"><img data-s="300,640" data-type="png" src="https://mmbiz.qlogo.cn/mmbiz/RHNE9AcLP6IbdINhbPcbXjQ72T9mSics6ibkP1UqcZMa1Lu8c4zSuruwSfRiceXWFjV44W6j6q1RgYYmnIxX3iapKQ/0?wx_fmt=png" data-ratio="1.6493506493506493" data-w="385"/><br/><span style="color: #888888;">做一个有态度的建筑人，建筑改变生活！</span></p>';
+
+
+    console.log(html);
+    return html;
+};
+
+
+
+
+//生成需要的html代码。标题
+Peixue.prototype.auto_html_title = function( ){
+
+
+    var html = '<p style="padding: 10px; font-family: 微软雅黑; text-align: center; font-size: 20px;color: rgb(255, 255, 255); line-height: 2em; background-color: rgb(0, 176, 80);">@  微文速递，为您速递价值！</p>';
+
+
+
+
+
+
+    console.log(html);
+    return html;
+
+};
+
 //生成需要的html代码。
 Peixue.prototype.insert_html = function( html1, html2 ){
 
@@ -277,6 +342,9 @@ Peixue.prototype.auto_html_icon = function( arg ){
 
 //自动生成陪学微信的计算逻辑。
 Peixue.prototype.auto_peixue = function( peixue_lists ){
+
+    this.peixue_html  = this.auto_html_title();
+
     var i = 0;
     //对微信数组中的内容进行遍历。
     for (i = 0; i < peixue_lists.length; i){
@@ -326,7 +394,7 @@ Peixue.prototype.auto_peixue = function( peixue_lists ){
             j = i;
             console.log("检查数量" + peixue_lists[j]);
 
-            while (peixue_lists[j].match( /检[验查]方法/ ) == null  && peixue_lists[j].match( /盖邦解读/ ) == null && peixue_lists[j].match( /友情赞助/) == null ){
+            while (peixue_lists[j].match( /^检[验查]方法/ ) == null  && peixue_lists[j].match( /盖邦解读/ ) == null && peixue_lists[j].match( /友情赞助/) == null ){
 
                 this.peixue_html += this.auto_html_quality ( peixue_lists[j] );
                 console.log("检查数量" + peixue_lists[j]);
@@ -338,10 +406,10 @@ Peixue.prototype.auto_peixue = function( peixue_lists ){
                 }
             }
             i = j;
-        }else if( peixue_lists[i].match( /检[验查]方法/ ) !== null ){
+        }else if( peixue_lists[i].match( /^检[验查]方法/ ) !== null ){
 
             j = i;
-            while (peixue_lists[j].match( /盖邦解读?/ ) == null && peixue_lists[j].match( /友情赞助?/ ) == null ){
+            while (peixue_lists[j].match( /盖邦解读?/ ) == null && peixue_lists[j].match( /友情赞助?/ ) == null && peixue_lists[j].match( /优质公众号?/ ) == null){
 
                 this.peixue_html += this.auto_html_method( peixue_lists[j] );
                 console.log(peixue_lists[j]);
@@ -359,7 +427,7 @@ Peixue.prototype.auto_peixue = function( peixue_lists ){
             j = i ;
 
 
-            while ( peixue_lists[j].match( /\d+\.\d+\s/ ) == null && peixue_lists[j].match( /继续下一条/ ) == null && peixue_lists[j].match( /友情赞助/ ) == null && peixue_lists[j].match( /相关条款/ ) == null ){
+            while ( peixue_lists[j].match( /\d+\.\d+\s/ ) == null && peixue_lists[j].match( /继续下一条/ ) == null && peixue_lists[j].match( /友情赞助/ ) == null && peixue_lists[j].match( /相关条款/ ) == null && peixue_lists[j].match( /优质公众号?/ ) == null){
 
                 this.peixue_html += this.auto_html_guide( peixue_lists[j] );
                 console.log('盖邦解读' + peixue_lists[j]);
@@ -376,7 +444,7 @@ Peixue.prototype.auto_peixue = function( peixue_lists ){
         }else if( peixue_lists[i].match( /相关条款?/ ) !== null ){
 
             j = i;
-            while (peixue_lists[j].match( /继续下一条/ ) == null && peixue_lists[j].match( /友情赞助/ ) == null && peixue_lists[j].match( /盖邦解读/ ) == null){
+            while (peixue_lists[j].match( /继续下一条/ ) == null && peixue_lists[j].match( /友情赞助/ ) == null && peixue_lists[j].match( /盖邦解读/ ) == null && peixue_lists[j].match( /优质公众号?/ ) == null ){
 
                 this.peixue_html += this.auto_html_other( peixue_lists[j] );
                 j++;
@@ -387,6 +455,15 @@ Peixue.prototype.auto_peixue = function( peixue_lists ){
                 }
             }
             i = j;
+
+        }else if (peixue_lists[i].match( /优质公众号/ ) !== null){
+
+
+            this.peixue_html += this.auto_html_gonghao(peixue_lists[i]);
+            i = i + 1;
+
+
+
 
         }else if( peixue_lists[i].match( /友情赞助?/ ) !== null ){
 
@@ -406,7 +483,7 @@ Peixue.prototype.auto_peixue = function( peixue_lists ){
 
             j = i;
             this.peixue_html += this.auto_html_icon(4);
-            while ( peixue_lists[j].match( /\d+\s/ ) == null &&  peixue_lists[j].match( /\d+\.\d+\.\d+\s/ ) == null &&  peixue_lists[j].match( /盖邦解读?/ ) == null  ){
+            while ( peixue_lists[j].match( /\d+\s/ ) == null &&  peixue_lists[j].match( /\d+\.\d+\.\d+\s/ ) == null &&  peixue_lists[j].match( /盖邦解读?/ ) == null &&  peixue_lists[j].match( /友情赞助?/ ) == null && peixue_lists[j].match( /优质公众号?/ ) == null ){
 
 
                 console.log("继续下一条" + peixue_lists[j]);
